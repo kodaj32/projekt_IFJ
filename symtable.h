6b3bbc3df645a54ce755263ceb50ce1b6c78d9c8
Tomas Sedo <tomassedo@gmail.com>
2022-12-06 22:28:26 +0100

    key is string, tdata_Type

--- symtable.h
+++ symtable.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 // Typ identifikatoru
 
@@ -8,10 +9,19 @@ typedef enum{
     FUNC,                  // funkcia(function)
 } tType;
 
+typedef enum{
+    INT,                   // premenna(variable)
+    FLOAT,                 // funkcia(function)
+    VOID,
+    UNDEFINED,
+    STRING,
+} tdata_Type;
+
 //Data v uzle
 
 typedef struct tData{
     tType type;                         // typ identifikatoru
+    tdata_Type data_type;               // datovy typ
     bool defined;                       // pozerame ci volana funkcia bola definovana
     bool called;                        // pozerame ci definovana funkcia bola volana
     struct bst_node *local_scope;       // pointer na lokalnu tabulku
@@ -20,15 +30,15 @@ typedef struct tData{
 
 // Uzol stromu
 typedef struct bst_node {
-  char key;               // kľúč
+  char *key;               // kľúč
   int value;              // hodnota
   struct bst_node *left;  // ľavý potomok
   struct bst_node *right; // pravý potomok
 } bst_node_t;
 
 void bst_init(bst_node_t **tree);
-int bst_insert(bst_node_t **tree, char key, int value);
-tData_t bst_search(bst_node_t *tree, char key, int *value);
-void bst_delete(bst_node_t **tree, char key);
+int bst_insert(bst_node_t **tree, char *key, tData_t value);
+tData_t bst_search(bst_node_t *tree, char *key, tData_t *value);
+void bst_delete(bst_node_t **tree, char *key);
 void bst_dispose(bst_node_t **tree);
 void bst_replace_by_rightmost(bst_node_t *target, bst_node_t **tree);
