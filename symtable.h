bee3c3a9e7a8e8af777e7d38c0a6a4f22d7bd5ba
Tomas Sedo <tomassedo@gmail.com>
2022-12-08 05:12:56 +0100

    Added comments

--- symtable.h
+++ symtable.h
@@ -3,46 +3,79 @@
 #include <string.h>
 #include "linked_list.h"
 
-// Typ identifikatoru
+// Type of identifier
 
 typedef enum{
-    VAR,                   // premenna(variable)
-    FUNC,                  // funkcia(function)
+    VAR,                   //variable
+    FUNC,                  //function
 } tType;
 
+// Data type
+
 typedef enum{
-    INT,                   // premenna(variable)
-    FLOAT,                 // funkcia(function)
+    INT,                   
+    FLOAT,                 
     VOID,
     UNDEFINED,
     STRING,
 } tdata_Type;
 
-//Data v uzle
+//Data in node
 
 typedef struct tData_t{
-    tType type;                         // typ identifikatoru
-    tdata_Type data_type;               // datovy typ
-    bool defined;                       // pozerame ci volana funkcia bola definovana
-    bool called;                        // pozerame ci definovana funkcia bola volana
-    struct bst_node *local_scope;       // pointer na lokalnu tabulku
-    int params;                         // pocet parametrov funkcie
-    LList *func_params;                 // parametre funkcie, zoznam
+    tType type;                         // type of identifier
+    tdata_Type data_type;               // data type
+    bool defined;                       // true if the function was defined, otherwise false
+    bool called;                        // true if function was called otherwise false
+    struct bst_node *local_scope;       // pointer to local table
+    int params;                         // count of function params
+    LList *func_params;                 // list of function params
 } *tData_t;
 
 // Uzol stromu
 typedef struct bst_node {
-  char *key;                // kľúč
-  tData_t value;            // hodnota
-  struct bst_node *left;    // ľavý potomok
-  struct bst_node *right;   // pravý potomok
+  char *key;                // key to determine where to insert, find or delete
+  tData_t value;            // data
+  struct bst_node *left;    // left child
+  struct bst_node *right;   // right child
 } bst_node_t;
 
+/**
+ * Initialization of binary search tree
+*/
 void bst_init(bst_node_t **tree);
+
+/**
+ * Inserting node into BST with key and storing data. Key decides by its value in ASCII if it goes left/right
+*/
 void bst_insert(bst_node_t **tree, char *key, tData_t value);
+
+/**
+ * Searching in BST with key and returns data
+*/
 tData_t bst_search(bst_node_t *tree, char *key, tData_t *value);
+
+/**
+ * Delete the node from the BST by its okay together with data
+*/
 void bst_delete(bst_node_t **tree, char *key);
+
+/**
+ * Free the whole tree
+*/
 void bst_dispose(bst_node_t **tree);
+
+/**
+ * Function used in delete. Find the rightmost node
+*/
 void bst_replace_by_rightmost(bst_node_t *target, bst_node_t **tree);
+
+/**
+ * Add predefined function into BST
+*/
 void add_funcs(bst_node_t **tree);
+
+/**
+ * Error printing to stderr when allocation fails
+*/
 void symtable_error();
